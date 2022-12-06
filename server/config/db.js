de74efe78464dacc326854f6beb24214677c11f5
juliaongohh <jojuliamari@gmail.com>
2022-12-06 01:09:40 -0500

    final commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
  {
- "URI":"mongodb://127.0.0.1:27017/contacts"
+ "URI":"mongodb://127.0.0.1:27017/contacts",
+ "URI":"mongodb+srv://juliaongohh:12345@cluster0.kaiokjq.mongodb.net/books?retryWrites=true&w=majority"
 }
